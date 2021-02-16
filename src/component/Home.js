35d141b1fc939da77f4e2ba7fdda9ca98a45bd73
Nina Yang <ssssrxdd@gmail.com>
2021-02-15 23:50:28 -0500

    aah

---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TitleBar from './TitleBar';
+
+class Home extends React.Component{
+
+    render(){
+
+        return (<>
+            <TitleBar/>
+            <p>Lobster</p>
+        </>)
+    }
+    
+}
+
+
+export default Home;
---
+++ src/component/TitleBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../style/titleStyle.css';
+class TitleBar extends React.Component{
+
+    render(){
+
+        
+        return (<>
+            <div className="titlebar-titlebar"> 
+                <div className="titlebar-title">🦞 Lobster Finance</div>
+            </div>
+        </>)
+    }
+    
+}
+
+
+export default TitleBar;
